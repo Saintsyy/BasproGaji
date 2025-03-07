ccb0cd1732b19ef457c1c2e8c879c7388fda7f6f
Saintsyy <saints.androni18@gmail.com>
2025-03-07 15:28:13 +0700

    Add files via upload

---
+++ Hasil-Gaji.py
@@ -0,0 +1,38 @@
+nama = input("Masukkan Nama : ")
+nik = input("Masukkan NIK : ")
+status = input("Masukkan Status (Pegawai Tetap/Honor) : ")
+golongan = input("Masukkan Golongan (A/B/C) : ")
+
+if status == "Tetap" or status == "Honor":
+    if golongan == "A" or golongan == "B" or golongan == "C":
+        if status == "Tetap":
+            gaji = 1000000
+            if golongan == "A":
+                bonus = 200000
+            elif golongan == "B":
+                bonus = 400000
+            else:
+                bonus = 550000
+        else:  # status == "Honor"
+            gaji = 750000
+            if golongan == "A":
+                bonus = 150000
+            elif golongan == "B":
+                bonus = 275000
+            else:
+                bonus = 480000
+
+        total_gaji = gaji + bonus
+
+        print("\n===Hasil===")
+        print("Nama: " + nama)
+        print("NIK: " + nik)
+        print("Status: " + status)
+        print("Golongan: " + golongan)
+        print("Gaji Pokok: Rp " + format(gaji, ','))
+        print("Bonus Golongan " + golongan + ": Rp " + format(bonus, ','))
+        print("Total Gaji: Rp " + format(total_gaji, ','))
+    else:
+        print("Golongan tidak valid!")
+else:
+    print("Status tidak valid!")
